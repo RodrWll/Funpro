b96cc4638348f3e5f10af74d9c2822f73a43b09a
Rodrigo Agurto <a20202021@pucp.edu.pe>
2024-02-21 13:26:39 -0500

    finished

--- Fibonacci.c
+++ Fibonacci.c
@@ -4,7 +4,7 @@ int isFibonacci(int n);
 int positionInFibonacci(int n);
 int totalFibonacciInRange(int start, int end);
 int numberIsSquare(int n);
-//int printGraph(int start, int end);
+void printGraph(int start, int end);
 
 int main()
 {
@@ -18,7 +18,7 @@ int main()
             printf("The limit of numbers in the range entered has been exceeded\n");
         }else{
             printf("The follow graph shows the number of Fibonacci numbers in the range entered\n");
-            //printGraph(start, end);
+            printGraph(start, end);
         }
     }
     return 0;
@@ -54,10 +54,21 @@ int positionInFibonacci(int n)
     int position = 0;
     for (int i = 0; i < n; i++)
     {
+        //case pos 0 fib number is 0
+        //case pos 1 fib number is 1
+        //case pos 2 fib number is 1
+        if(c==1){
+            position=2;
+        }
         c = a + b;
         a = b;
         b = c;
         position++;
+        
+        if (c == n)
+        {
+            break;
+        }
     }
     return position;
 }
@@ -66,12 +77,13 @@ int totalFibonacciInRange(int start, int end)
     int count = 0;
     for (int i = start; i <= end; i++)
     {
-        if (isFibonacci(i))
+        if(i==1){
+            count+=2;
+        }else if (isFibonacci(i))
         {
             count++;
         }
     }
-    printf("%d\n",count);
     return count;
 }
 int numberIsSquare(int n)
@@ -85,4 +97,71 @@ int numberIsSquare(int n)
         }
     }
     return 0;
+}
+void printGraph(int start, int end)
+{
+    //prints a graph of the position of the Fibonacci numbers and the Fibonacci numbers in the range
+    //firt we need to know the total of Fibonacci numbers in the range
+    int totalFibonacci = totalFibonacciInRange(start, end);
+    //then we need to know the limit of the graph
+    int maxLimit = 0;
+    int posmaxLimit = 0;
+    int minLimit = 0;
+    int posminLimit = 0;
+    for (int i = end; i >= start; i--)
+    {
+        if (isFibonacci(i))
+        {
+            maxLimit = i;
+            posmaxLimit = positionInFibonacci(i);
+            break;
+        }
+    }
+    for (int i = start; i <= end; i++)
+    {
+        if (isFibonacci(i))
+        {
+            minLimit = i;
+            posminLimit = positionInFibonacci(i);
+            break;
+        }
+    }
+
+
+    printf ("\t"); //"space to position (n):"
+    for(int i = 0; i < totalFibonacci; i++){
+        printf("\t");
+    }
+    printf("Fibonacci(n) \n");
+    int o=0;
+    int m=0;
+    int l=totalFibonacci;
+    for(int i = maxLimit; i >= 1; i--){
+        printf("\t");
+        if(isFibonacci(i)){
+                o++;           
+        }
+        for(int j = 0; j < totalFibonacci; j++){   
+            if(m!=l-o){
+                printf("\t");
+                m++;
+            }else{
+                printf("|\t");
+                
+            }
+        }
+        m=0;
+            printf("%d\n", i);
+    }
+    printf("position(n)"); 
+    int k= minLimit;
+    for(int i = 0; i < totalFibonacci; i++){
+        if(k==1 || k==2){
+            printf("\t1 o 2");
+            k+=2;
+        }else{
+            printf("\t%d", k);
+            k++;
+        }       
+    }
 }
